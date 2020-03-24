626867541e6e659c3cb43cf8265ec1c8d8aa7701
DiegoRamires <goncalves.ramires@gmail.com>
2020-03-24 16:02:43 -0300

    create and list incidents routes and methods

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 
 import OngController from "./controllers/OngController";
+import IncidentController from "./controllers/IncidentController";
 
 const routes = new Router();
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
+
+routes.get("/incidents", IncidentController.index);
+routes.post("/incidents", IncidentController.create);
 
 export default routes;
