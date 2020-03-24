2d73b1a272d1ba3e30194c9b5b81435babc3e777
DiegoRamires <goncalves.ramires@gmail.com>
2020-03-24 17:57:15 -0300

    feature profile. listing all specific ONGs incidents

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,14 @@ import { Router } from "express";
 
 import OngController from "./controllers/OngController";
 import IncidentController from "./controllers/IncidentController";
+import ProfileController from "./controllers/ProfileController";
 
 const routes = new Router();
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
+
+routes.get("/profile", ProfileController.index);
 
 routes.get("/incidents", IncidentController.index);
 routes.post("/incidents", IncidentController.create);
